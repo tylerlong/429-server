7b1bd0a73e6486a4439712cd518f883d7120332e
Tyler Long <tyler4long@gmail.com>
2021-12-08 15:50:29 -0800

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+import express from 'express';
+
+const app = express();
+
+app.get('*', (req, res) => {
+  res.status(429);
+  res.send({hello: 'world'});
+});
+app.post('*', (req, res) => {
+  res.status(429);
+  res.send({hello: 'world'});
+});
+
+app.listen(8000, () => {
+  console.log('Example app listening at http://localhost:8000');
+});
